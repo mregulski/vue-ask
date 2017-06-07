cccda3398f550e74c7c9eca72e5df0cca1849f58
Marcin Regulski <maarcin.regulski@gmail.com>
2017-06-07 15:16:22 +0200

    init project config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+path = require('path')
+
+module.exports = {
+    entry: './app/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                    }
+                    // other vue-loader options go here
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    devtool: "cheap-eval-source-map",
+    devServer: { inline: true}
+};
